7051d0f99d22bd6bba2615d692e1bd9eb245fc0f
Gourab Choudhury <129882446+gourab9817@users.noreply.github.com>
2024-12-11 23:06:35 +0530

    Update settings.py

--- Flipkart_Grid_6_0/settings.py
+++ Flipkart_Grid_6_0/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'Flipkart_Grid_6_0.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
